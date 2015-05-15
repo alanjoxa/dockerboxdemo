8c01863946e82f70bdbf0941a65ff45254e72ed8
Alan Joseph <alan.joseph@myntra.com>
2015-05-16 00:09:36 +0530

    Added the website

---
+++ website/website.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http');
+var morgan = require('morgan')
+
+var request = require('request');
+var service = 'http://todoservice.dockerbox.in/';
+
+http.globalAgent.maxSockets = Infinity;
+
+var app = express();
+
+// view engine setup
+app.set('views', './view');
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(morgan('combined'));
+
+app.get('/', function(req, res, next){
+	request(service, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    res.render('index', {todo : JSON.parse(body)});
+	  } else {
+	  	res.json('service error');
+	  }
+	});
+});
+
+app.post('/', function(req, res, next){	
+	request.post({url : service + 'add', json: req.body}, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    res.redirect('/');
+	  } else {
+	  	res.json('service error');
+	  }
+	});
+});
+
+app.set('port', process.env.PORT || 8000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
